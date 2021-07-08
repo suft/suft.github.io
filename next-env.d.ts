3377dcb24489cd0b5e5a3fd9b6c8a01a6a50ce62
Sufien Tout <sufientout@gmail.com>
2021-07-08 15:50:53 -0400

    Add Google Analytics

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,3 +1,6 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
 /// <reference types="next/image-types/global" />
+/// <reference types="gtag.js" />
+
+declare module 'gtag.js'
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,57 @@
+import { useEffect, FC } from 'react'
+import type { AppProps, NextWebVitalsMetric } from 'next/app'
+import { useRouter } from 'next/router'
+import * as gtag from '../utils/analytics'
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+
+const Noop: FC = ({ children }) => <>{children}</>
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
+  const LayoutNoop = (Component as any).LayoutNoop || Noop
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => gtag.pageview(url)
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => router.events.off('routeChangeComplete', handleRouteChange)
+  }, [router.events])
+  return (
+    <>
+      <LayoutNoop pageProps={pageProps}>
+        <Component {...pageProps} />
+      </LayoutNoop>
+    </>
+  )
+}
+
+export const reportWebVitals = (metric: NextWebVitalsMetric): void => {
+  switch (metric.name) {
+    case 'FCP':
+      console.log('FCP: ', metric)
+      break
+    case 'LCP':
+      console.log('LCP: ', metric)
+      break
+    case 'CLS':
+      console.log('CLS: ', metric)
+      break
+    case 'FID':
+      console.log('FID: ', metric)
+      break
+    case 'TTFB':
+      console.log('TTFB: ', metric)
+      break
+    case 'Next.js-hydration':
+      console.log('Next.js-hydration: ', metric)
+      break
+    case 'Next.js-route-change-to-render':
+      console.log('Next.js-route-change-to-render: ', metric)
+      break
+    case 'Next.js-render':
+      console.log('Next.js-render: ', metric)
+      break
+    default:
+      break
+  }
 }
 
 export default App
