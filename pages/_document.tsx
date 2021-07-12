91659a34fa645482f75c7ac3a7ddcdc7f7b0e758
Sufien Tout <sufientout@gmail.com>
2021-07-12 16:07:36 -0400

    Move google verification to document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,14 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
-class MyDocument extends Document {
+class WebDocument extends Document {
   render(): React.ReactElement {
     return (
       <Html>
-        <Head />
+        <Head>
+          <meta
+            name="google-site-verification"
+            content="N8aF7i2DswJjIfs6wdT7v-Q1iwzyCBemz5Ec9TDeaFw" />
+        </Head>
         <body className="bg-white dark:bg-gray-900 text-gray-900 dark:text-gray-50">
           <Main />
           <NextScript />
