88d553d1fac705f56f00c0c1eed117078bd8a71e
suft <suft@users.noreply.github.com>
2021-07-18 16:28:44 +0000

    Deploying to gh-pages from @ suft/suft.github.io@21ed9936cafa10b3ecdbc3e58f3da49943ca5e95 🚀

--- _next/static/mBTnNyiOWmArXxCGdPXId/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/75fc9c18-5c1929f66343f0a636cd.js","static/chunks/14-44c60e023a82688930e0.js","static/chunks/pages/index-d0c6f50df4becd09d6eb.js"],"/_error":["static/chunks/pages/_error-82a806cd39f8ab3dc3ac.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
