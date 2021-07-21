3a08de8170510e38eaac26ad69a9ae92a7f8b38f
Sufien Tout <sufientout@gmail.com>
2021-07-21 18:21:53 -0400

    Add made with love to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import moment from 'moment'
+import { E } from './Tags'
 
 const Footer = () => {
   return (
-    <footer className="pt-12 flex justify-center">
-      <small>Copyright &copy;{moment().year()}, Sufien Tout</small>
+    <footer className="pt-12 flex justify-center text-center">
+      <p>
+        <small>
+          Made with <E>❤️</E> in <strong>Barrie</strong>
+        </small>
+        <br />
+        <small>
+          Copyright &copy; {moment().year()} Sufien Tout
+        </small>
+      </p>
     </footer>
   )
 }
