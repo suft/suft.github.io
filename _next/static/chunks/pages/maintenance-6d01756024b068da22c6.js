69072869075bef4c3292c6e234fbbc3d17f06a97
suft <sufientout@gmail.com>
2021-12-03 03:21:56 +0000

    Deploying to gh-pages from @ suft/suft.github.io@ab0dec91e1b9f9069e0fa5c13b41cb4ba57f54a9 🚀

---
+++ _next/static/chunks/pages/maintenance-6d01756024b068da22c6.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[627],{7372:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/maintenance",function(){return u(4228)}])}},function(n){n.O(0,[774,888,179],(function(){return _=7372,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
